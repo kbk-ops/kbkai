948b0d4795fb5208136ac109fd9706d45eab14b3
kbk-ops <yeesphilippines@gmail.com>
2026-01-21 16:12:28 +0800

    Update auth.js

--- collector/dashboard/auth.js
+++ collector/dashboard/auth.js
@@ -1,1 +1,12 @@
+const isAuth = sessionStorage.getItem("auth");
+const expiry = sessionStorage.getItem("expiry");
 
+if (!isAuth || !expiry || Date.now() > expiry) {
+  sessionStorage.clear();
+  window.location.href = "https://kbk-ops.github.io/OrganizationFund/collector";
+}
+
+setTimeout(() => {
+  sessionStorage.clear();
+  window.location.href = "https://kbk-ops.github.io/OrganizationFund/collector";
+}, expiry - Date.now());
