1c5125c8f8977f0fcec3ab14b930104506683bcc
kbk-ops <yeesphilippines@gmail.com>
2026-01-21 14:43:41 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("officersBtn").onclick = function () {
+  window.location.href = "https://kbk-ops.github.io/OrganizationFund/collector";
+};
+
+document.getElementById("membersBtn").onclick = function () {
+  window.location.href = "https://kbk-ops.github.io/OrganizationFund/user";
+};
