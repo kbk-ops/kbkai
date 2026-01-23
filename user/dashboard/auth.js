8fa2978b881aa9cf796254a24d1146191857af84
kbk-ops <yeesphilippines@gmail.com>
2026-01-23 16:13:27 +0800

    Update auth.js

--- user/dashboard/auth.js
+++ user/dashboard/auth.js
@@ -1,1 +1,26 @@
+const SESSION_TIME = 1 * 60 * 1000;
+const auth = sessionStorage.getItem("auth");
+const lastActivity = sessionStorage.getItem("lastActivity");
 
+if (!auth || !lastActivity) {
+  sessionStorage.clear();
+  window.location.href = "https://kbk-ops.github.io/OrganizationFund";
+}
+
+// update activity on any interaction
+["click","mousemove","keydown","touchstart","scroll"].forEach(evt => {
+  document.addEventListener(evt, () => {
+    sessionStorage.setItem("lastActivity", Date.now());
+  });
+});
+
+// idle checker
+setInterval(() => {
+  const now = Date.now();
+  const last = parseInt(sessionStorage.getItem("lastActivity"));
+
+  if (now - last > SESSION_TIME) {
+    sessionStorage.clear();
+    window.location.href = "https://kbk-ops.github.io/OrganizationFund";
+  }
+}, 10000);
