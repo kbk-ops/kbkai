a84d92e8358e8ca5ded16c7e07b6c4785af212f0
kbk-ops <yeesphilippines@gmail.com>
2026-01-25 14:57:34 +0800

    Update script.js

--- user/dashboard/perks/script.js
+++ user/dashboard/perks/script.js
@@ -18,10 +18,3 @@ cards.forEach(card=>{
 function closeFullscreen(){
   fullscreen.classList.remove("active");
 }
-
-function showTab(id){
-  document.querySelectorAll(".tab-content")
-    .forEach(t => t.classList.remove("active"));
-
-  document.getElementById(id).classList.add("active");
-}
