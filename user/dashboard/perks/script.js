b1033c1879db570f66c0c08019a8fe10ba116012
kbk-ops <yeesphilippines@gmail.com>
2026-01-25 12:16:54 +0800

    Update script.js

--- user/dashboard/perks/script.js
+++ user/dashboard/perks/script.js
@@ -1,11 +1,17 @@
 const cards = document.querySelectorAll(".card");
 const fullscreen = document.getElementById("fullscreen");
 const fsText = document.getElementById("fs-text");
+const fsInner = document.getElementById("fs-inner");
 
 cards.forEach(card=>{
   card.addEventListener("click",()=>{
-    fsText.textContent = card.dataset.text;
-    fullscreen.classList.add("active");
+    card.classList.add("flipped");
+
+    setTimeout(()=>{
+      fsText.textContent = card.dataset.text;
+      fullscreen.classList.add("active");
+      card.classList.remove("flipped");
+    },600);
   });
 });
 
