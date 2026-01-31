da9ddf88e519c1734fff70836b58a4e46719881d
kbk-ops <yeesphilippines@gmail.com>
2026-02-01 00:01:12 +0800

    Update nav.js

--- user/dashboard/nav.js
+++ user/dashboard/nav.js
@@ -1,6 +1,6 @@
 function goDashboard(tab = "homeTab") {
   sessionStorage.setItem("activeTab", tab);
-  location.replace = "https://kbk-ops.github.io/OrganizationFund/user/dashboard/";
+  location.href = "https://kbk-ops.github.io/OrganizationFund/user/dashboard/";
 }
 
 function initDashboardTabs() {
